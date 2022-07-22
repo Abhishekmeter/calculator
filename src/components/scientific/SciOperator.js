dc5ba2e882866e461fb1982c9c37c41e6c6480a9
Abhishekmeter <30804824+Abhishekmeter@users.noreply.github.com>
2022-07-22 06:10:48 -0500

    Update SciOperator.js

--- src/components/scientific/SciOperator.js
+++ src/components/scientific/SciOperator.js
@@ -1,13 +1,18 @@
 import Square from "./Square";
 import SquareRoot from "./SquareRoot";
+import Reverse from "./Reverse";
 
 import styles from "./SciOperator.module.css";
+import Exponent from "./Exponent";
 
 function SciOperator(props) {
   return (
     <div className={styles.scioperator}>
-      <Square onInput={props.onInput} value="sqr"/>
-      <SquareRoot />
+      <button onClick={() => props.onReset()}>C</button>
+      <Square onInput={props.onSquare} calc={props.calc} />
+      <SquareRoot onInput={props.onSquareRoot} calc={props.calc} />
+      <Reverse onInput={props.onReverse} calc={props.calc} />
+      <Exponent onInput={props.exponent}/>
     </div>
   );
 }

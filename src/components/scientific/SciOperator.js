2888b4b116b4d054fe3522e5f75879303aa04511
Abhishekmeter <30804824+Abhishekmeter@users.noreply.github.com>
2022-07-25 04:03:24 -0500

    Update SciOperator.js

--- src/components/scientific/SciOperator.js
+++ src/components/scientific/SciOperator.js
@@ -4,15 +4,41 @@ import Reverse from "./Reverse";
 
 import styles from "./SciOperator.module.css";
 import Exponent from "./Exponent";
+import ExponentPow from "./ExponentPow";
+import Sin from "./Sin";
+import Cos from "./Cos";
+import Tan from "./Tan";
 
 function SciOperator(props) {
   return (
     <div className={styles.scioperator}>
-      <button onClick={() => props.onReset()}>C</button>
-      <Square onInput={props.onSquare} calc={props.calc} />
-      <SquareRoot onInput={props.onSquareRoot} calc={props.calc} />
-      <Reverse onInput={props.onReverse} calc={props.calc} />
-      <Exponent onInput={props.exponent}/>
+      <div>
+        <button onClick={() => props.onReset()}>C</button>
+      </div>
+      <div>
+        <Square onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <SquareRoot onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <Reverse onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <Exponent onInput={props.onOperation} />
+      </div>
+      <div>
+        <ExponentPow onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <Sin onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <Cos onInput={props.onOperation} calc={props.calc} />
+      </div>
+      <div>
+        <Tan onInput={props.onOperation} calc={props.calc} />
+      </div>
     </div>
   );
 }
